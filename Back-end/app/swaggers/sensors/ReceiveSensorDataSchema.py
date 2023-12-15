c0929cb29e0cd5c1a9c5e5615e044a23261fdf74
heCarlo <carlosgfkp@outlook.com>
2023-12-15 14:35:01 -0300

    refactor: corrigindo swagger

--- Back-end/app/swaggers/sensors/ReceiveSensorDataSchema.py
+++ Back-end/app/swaggers/sensors/ReceiveSensorDataSchema.py
@@ -22,7 +22,7 @@ sensor_data_response_properties = {
     'created_at': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATETIME),
 }
 
-# Defines various response statuses and their schemas
+# Defines various response statuses and their schemas for sensor data
 sensor_data_responses = {
     201: 'Data received successfully!',
     200: openapi.Schema(
@@ -38,7 +38,7 @@ sensor_data_responses = {
     500: 'Error processing data',
 }
 
-# Defines Swagger schemas for different HTTP methods
+# Defines Swagger schemas for different HTTP methods for sensor data
 receive_sensor_data_post_schema = swagger_auto_schema(
     request_body=sensor_data_request_body,
     responses=sensor_data_responses,
@@ -59,4 +59,46 @@ receive_sensor_data_delete_schema = swagger_auto_schema(
 
 receive_sensor_data_get_by_id_schema = swagger_auto_schema(
     responses=sensor_data_responses,
+)
+
+# Defines the request body schema for secury mode data
+secury_mode_request_body = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'secury_mode': openapi.Schema(type=openapi.TYPE_BOOLEAN),
+    },
+    required=['secury_mode']
+)
+
+# Defines the response properties for secury mode data
+secury_mode_response_properties = {
+    'id': openapi.Schema(type=openapi.TYPE_INTEGER),
+    'secury_mode': openapi.Schema(type=openapi.TYPE_BOOLEAN),
+    'created_at': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_DATETIME),
+}
+
+# Defines various response statuses and their schemas for secury mode data
+secury_mode_responses = {
+    201: 'Secury mode data received successfully!',
+    200: openapi.Schema(
+        type=openapi.TYPE_ARRAY,
+        items=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties=secury_mode_response_properties,
+        )
+    ),
+    204: 'Secury mode data deleted successfully!',
+    400: 'Invalid data',
+    404: 'Secury mode data not found',
+    500: 'Error processing secury mode data',
+}
+
+# Defines Swagger schemas for different HTTP methods for secury mode data
+secury_mode_retrieve_update_destroy_schema = swagger_auto_schema(
+    responses=secury_mode_responses,
+)
+
+secury_mode_create_schema = swagger_auto_schema(
+    request_body=secury_mode_request_body,
+    responses=secury_mode_responses,
 )
--- Back-end/app/views/sensors/receiveSensorDataView.py
+++ Back-end/app/views/sensors/receiveSensorDataView.py
@@ -6,8 +6,21 @@ from ...models.sensors.sensorDataEntity import SensorData
 from ...models.sensors.securyModeEntity import SecuryMode
 from ...serializers.sensorDataSerializer import SensorDataSerializer
 from ...serializers.securyModeSerializer import SecuryModeSerializer
+from ...swaggers.sensors.ReceiveSensorDataSchema import (
+    receive_sensor_data_delete_schema,
+    receive_sensor_data_get_by_id_schema,
+    receive_sensor_data_get_schema,
+    receive_sensor_data_post_schema,
+    receive_sensor_data_put_schema,
+    )
+from ...swaggers.sensors.ReceiveSensorDataSchema import (
+    secury_mode_retrieve_update_destroy_schema,
+    secury_mode_create_schema
+)
+
 
 class ReceiveSensorDataPostView(APIView):
+    @receive_sensor_data_post_schema
     def post(self, request, *args, **kwargs):
         serializer = SensorDataSerializer(data=request.data)
         if serializer.is_valid():
@@ -16,12 +29,14 @@ class ReceiveSensorDataPostView(APIView):
         return Response({"status": "Invalid data", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 class ReceiveSensorDataGetView(APIView):
+    @receive_sensor_data_get_schema
     def get(self, request, *args, **kwargs):
         sensor_data = SensorData.objects.order_by('-id')[:7]
         serializer = SensorDataSerializer(sensor_data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class ReceiveSensorDataPutView(APIView):
+    @receive_sensor_data_put_schema
     def put(self, request, *args, **kwargs):
         instance = get_object_or_404(SensorData, id=kwargs.get("pk"))
         serializer = SensorDataSerializer(instance, data=request.data)
@@ -31,25 +46,29 @@ class ReceiveSensorDataPutView(APIView):
         return Response({"status": "Invalid data", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 class ReceiveSensorDataDeleteView(APIView):
+    @receive_sensor_data_delete_schema
     def delete(self, request, *args, **kwargs):
         instance = get_object_or_404(SensorData, id=kwargs.get("pk"))
         instance.delete()
         return Response({"status": "Data deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
 
 class ReceiveSensorDataGetByIdView(APIView):
+    @receive_sensor_data_get_by_id_schema
     def get(self, request, pk, *args, **kwargs):
         instance = get_object_or_404(SensorData, id=pk)
         serializer = SensorDataSerializer(instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class SecuryModeCreateView(APIView):
+    @secury_mode_create_schema
     def post(self, request, *args, **kwargs):
         serializer = SecuryModeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+    @secury_mode_create_schema
     def put(self, request, *args, **kwargs):
         instance = get_object_or_404(SecuryMode, id=kwargs.get("pk"))
         serializer = SecuryModeSerializer(instance, data=request.data)
@@ -59,11 +78,13 @@ class SecuryModeCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SecuryModeRetrieveUpdateDestroyView(APIView):
+    @secury_mode_retrieve_update_destroy_schema
     def get(self, request, pk, *args, **kwargs):
         secury_mode = get_object_or_404(SecuryMode, pk=pk)
         serializer = SecuryModeSerializer(secury_mode)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
+    @secury_mode_retrieve_update_destroy_schema
     def put(self, request, pk, *args, **kwargs):
         secury_mode = get_object_or_404(SecuryMode, pk=pk)
         serializer = SecuryModeSerializer(secury_mode, data=request.data)
@@ -71,7 +92,8 @@ class SecuryModeRetrieveUpdateDestroyView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
+    @secury_mode_retrieve_update_destroy_schema
     def delete(self, request, pk, *args, **kwargs):
         secury_mode = get_object_or_404(SecuryMode, pk=pk)
         secury_mode.delete()
